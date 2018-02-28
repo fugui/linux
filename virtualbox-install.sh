03b93f12ea7fdb6d7b915e4042c317148cb402ce
Gui Fu <szfugui@gmail.com>
2018-02-28 11:39:02 -0500

    Create virtualbox-install.sh

---
+++ virtualbox-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cat >> /etc/apt/sources.list <<EOF
+deb https://download.virtualbox.org/virtualbox/debian xenial contrib
+EOF
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- |  apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- |  apt-key add -
+
+apt-get update
+
+apt-get install -y virtualbox-5.2
